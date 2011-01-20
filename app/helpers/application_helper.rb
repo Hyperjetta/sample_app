bd26202d17353acddbde61eff46e805e52cbf657
Jason Mallet <jmalletp@gmail.com>
2011-01-20 10:43:13 -0500

    Initial commit

---
+++ app/helpers/application_helper.rb
@@ -0,0 +1,12 @@
+module ApplicationHelper
+	
+	#Return a title on a per-page basis
+	def title
+		base_title = "Rails Tutorial"
+		if @title.nil?
+			base_title
+		else
+			"#{base_title} | #{@title}"
+		end
+	end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = '0fb476b28bde3e89eb740208440f4465db82eab38c080b1a29895690419420e64b29c575434aa8716e587434076408b35cf18c8707805bdc2eeba490f241652b'
